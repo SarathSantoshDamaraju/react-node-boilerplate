d7699d6b8718814a83505a7ff02b1e539e211872
Sarath Damaraju <sarathdamaraju@hotmail.com>
2017-08-31 11:58:11 +0530

    Seperated Build and run Scripts

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 4444;
 
 app.listen(port);
 console.log(`Listening at http://localhost:${port}`);
